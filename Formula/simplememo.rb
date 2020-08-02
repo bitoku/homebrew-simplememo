c298d1de402d6fc7884090cae4749e3e18e6e610
Ayato Tokubi <tokubi.ayato@gmail.com>
2020-08-02 17:54:00 +0900

    v0.1.9

--- Formula/simplememo.rb
+++ Formula/simplememo.rb
@@ -1,9 +1,9 @@
 class Simplememo < Formula
   desc "very minimal memo app in Rust"
   homepage "https://github.com/bitoku/memo"
-  url "https://github.com/bitoku/memo/releases/download/v0.1.7/simplememo-x86_64-apple-darwin.tar.gz"
-  sha256 "d09a182822e8774f4141607e2e1059f59d1e2a368bd76b88cbceac75038f72cd"
-  version "0.1.7"
+  url "https://github.com/bitoku/memo/releases/download/v0.1.9/simplememo-x86_64-apple-darwin.tar.gz"
+  sha256 "c1da6746a5b1ce924440467a97f9be72baca22749f292a841c46f07471c46f57"
+  version "0.1.9"
 
   def install
     bin.install "simplememo" => "memo"
